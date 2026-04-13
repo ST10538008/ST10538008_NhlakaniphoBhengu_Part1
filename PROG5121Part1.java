a5557c3f8c6adc2b19037633d22663a749f2626d
ST10538008 <ST10538008@rcconnect.edu.za>
2026-04-13 18:09:23 +0200

    Add files via upload

---
+++ PROG5121Part1.java
@@ -0,0 +1,166 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121part1;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+/**
+ *
+ * @author Student
+ */
+public class PROG5121Part1 {
+    //Method to check username validation
+    public static boolean checkUserName(String username){
+        return username.length()<=5 && username.contains("_");
+    }
+    
+    //Method to check the password complexity
+    public static boolean checkPasswordComplexity(String password){
+        String regex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$";
+        return Pattern.matches(regex, password);  
+    }
+    
+    //Method to check cellphone number
+    //It must start start with an international code (+27) followed by 9 characters in total must have 12 characters
+    public static boolean checkCellPhoneNumber(String cellNumber){
+        String regex = "^\\+27\\d{9}$";
+        return Pattern.matches(regex, cellNumber);
+    }
+    
+    //Method to register the user
+    public static String registerUser(String username, String password){
+        //check username
+        if (!checkUserName(username)){
+            return "Username is not correct. Must have _ and max 5 chars.";
+        }
+        
+        //check password
+        if (!checkPasswordComplexity(password)){
+            return "Password is not correct. Must have 8+ chars, capital letter, number, and special char.";
+        }
+        //check if both conditions are correct
+        return "User has been registered successfully.";
+    }
+    
+    //Method to check login details
+    public static boolean loginUser(String enteredUsername, String enteredPassword, String storedUsername, String storedPassword){
+        //check username if its entered correctly
+        return enteredUsername.equals(storedUsername) && enteredPassword.equals(storedPassword);
+        }
+    
+    //Show login message
+    public static String returnLoginStatus(boolean loginSuccess, String firstName, String lastName){
+        if (loginSuccess){
+            return "\nWelcome " + firstName + " " + lastName + " it is great to see you again.";
+        }
+        else{
+            return "Username or password incorrect, please try again.";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String username;
+        String password;
+        String cellNumber;
+        
+        boolean validUsername;
+        boolean validPassword;
+        boolean validCellNumber;
+        
+        //Register User
+        System.out.println("Register User details");
+        
+        //loop until valid username
+        while (true) { 
+
+            System.out.println("Enter Username (must contain '_' and max 5 chars): "); 
+
+            username = input.nextLine(); 
+
+            if (checkUserName(username)) break; 
+
+            System.out.println("Invalid username. Example: user_");
+        }
+        
+        
+        //loop through password characters
+        while (true) { 
+
+            System.out.println("Enter Password (8+ chars, 1 capital, 1 number, 1 special char): "); 
+
+            password = input.nextLine(); 
+
+            if (checkPasswordComplexity(password)) break; 
+
+            System.out.println("Invalid password. Example: Password1!");
+        }
+        
+        //loop until valid cellphone number
+        while (true) { 
+
+            System.out.println("Enter Cell Phone (+27 followed by 9 digits): "); 
+
+            cellNumber = input.nextLine(); 
+
+            if (checkCellPhoneNumber(cellNumber)) { 
+
+                System.out.println("Cell phone number successfully captured."); 
+                break;
+            }
+            else {
+                System.out.println("Invalid cellphone number. example: +27831234567");
+            }
+        }
+        
+        
+        //Call registerUser after the loop to display the final message
+        System.out.println("User has been registered successfully.");
+        
+        
+        //Login Section
+        System.out.println("\nLogin");
+        
+        
+        //Login user loop
+        String enteredUsername;
+        String enteredPassword;
+        boolean loginSuccess;
+        
+        do {
+            System.out.println("Enter username to login: ");
+            enteredUsername = input.nextLine().trim();
+            
+            System.out.println("Enter password to login: ");
+            enteredPassword = input.nextLine().trim();
+        
+        //Check login
+            loginSuccess = loginUser(enteredUsername, enteredPassword, username, password);
+            
+            if (!loginSuccess) {
+                System.out.println("Username or password incorrect, please try again.");
+            }
+        }
+        while (!loginSuccess);
+        
+        System.out.println("Login successful");
+        
+        //After Successful login
+        System.out.println("Enter first name: ");
+        String firstName = input.nextLine();
+        
+        System.out.println("Enter last name: ");
+        String lastName = input.nextLine();
+        
+        //Display welcome meassage 
+        System.out.println(returnLoginStatus(loginSuccess, firstName, lastName));
+    }
+}
+
+
+
+
+
+
